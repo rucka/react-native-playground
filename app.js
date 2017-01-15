7edc189594c46c923ac61dfe15795625d758e5c9
rucka <rucka@tiscalinet.it>
2017-01-15 12:45:31 +0100

    changed layout

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ import { Hello, Greeting, Bananas, Blink } from './components';
 export class App extends Component {
   render() {
     return (
+      /*
       <View style={{alignItems: 'center', marginVertical: 30}}>
         <Greeting name='Rexxar' />
         <Greeting name='Jaina' />
         <Greeting name='Valeera' />
         <Bananas />
         <Blink text='I love to blink' />
+      </View>*/
+
+       <View style={{flex: 1}} >
+        <View style={{flex: 0.1}} />
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} >
+          <Greeting name='Rexxar' />
+          <Greeting name='Jaina' />
+          <Greeting name='Valeera' />
+        </View>
+        <View style={{alignItems: 'center', flex: 1, backgroundColor: 'skyblue'}} >
+          <Bananas />
+        </View>
+        <View style={{flex: 1, backgroundColor: 'steelblue'}} >
+          <Blink text='I love to blink' />
+        </View>
       </View>
     );
   }
